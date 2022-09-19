3a045066723b2f72a1b66e5d3ac5ade3de2cd697
Aleksander <aleksandros-777@mail.ru>
2022-09-19 10:36:16 +0300

    added new examples

--- canvasUsage/app.js
+++ canvasUsage/app.js
@@ -13,13 +13,18 @@ const canvasWithPath = document.getElementById("path");
 if (canvasWithPath.getContext) {
   const ctx = canvasWithPath.getContext("2d");
 
+  ctx.fillStyle = "blue";
   ctx.beginPath();
-  ctx.moveTo(20, 20);
-  ctx.lineTo(20, 70);
-  ctx.lineTo(150, 70);
-  ctx.lineTo(150, 20);
-  ctx.closePath();
-  ctx.stroke();
+  ctx.moveTo(150, 20);
+  ctx.lineTo(200, 65);
+  ctx.lineTo(150, 110);
+  ctx.lineTo(100, 65);
+  ctx.fill();
+
+  ctx.fillStyle = "white";
+  ctx.beginPath();
+  ctx.arc(150, 65, 20, 0, Math.PI * 2);
+  ctx.fill();
 }
 
 const canvasWithRound = document.getElementById("circle");
@@ -51,8 +56,44 @@ if (berzierCanvas.getContext) {
   ctx.lineTo(150, 70);
   ctx.quadraticCurveTo(175, 50, 200, 70);
   ctx.lineTo(200, 100);
-
   ctx.closePath();
   ctx.fill();
 }
 
+const curveCanvas = document.getElementById("berzier-curve");
+
+if (curveCanvas.getContext) {
+  const ctx = curveCanvas.getContext("2d");
+  ctx.beginPath();
+  ctx.moveTo(75, 40);
+  ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+  ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+  ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+  ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+  ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+  ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+
+  ctx.stroke();
+}
+
+const canvasStar = document.getElementById("red-star");
+
+if (canvasStar.getContext) {
+  const ctx = canvasStar.getContext("2d");
+
+  ctx.fillStyle = "red";
+  ctx.beginPath();
+  ctx.moveTo(150, 10);
+  ctx.lineTo(170, 60);
+  ctx.lineTo(220, 60);
+  ctx.lineTo(180, 80);
+  ctx.lineTo(195, 120);
+  ctx.lineTo(150, 90);
+  ctx.lineTo(110, 120);
+  ctx.lineTo(125, 85);
+  ctx.lineTo(80, 60);
+  ctx.lineTo(130, 60);
+  ctx.closePath();
+
+  ctx.fill();
+}
